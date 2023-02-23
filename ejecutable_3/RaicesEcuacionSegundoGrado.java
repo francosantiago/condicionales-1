7162a06d2704973765877129f9996948eb9ecc04
Santiago Darío Franco Ruiz <102566762+francosantiago@users.noreply.github.com>
2023-02-23 06:46:04 -0500

    Update RaicesEcuacionSegundoGrado.java

--- ejecutable_3/RaicesEcuacionSegundoGrado.java
+++ ejecutable_3/RaicesEcuacionSegundoGrado.java
@@ -8,20 +8,16 @@ public class RaicesEcuacionSegundoGrado
     public static void main(String[] args) 
     {
 
-        // Solicitamos los coeficientes de la ecuación de segundo grado
         String aStr = JOptionPane.showInputDialog("Ingrese el coeficiente 'a' de la ecuación de segundo grado:");
         String bStr = JOptionPane.showInputDialog("Ingrese el coeficiente 'b' de la ecuación de segundo grado:");
         String cStr = JOptionPane.showInputDialog("Ingrese el coeficiente 'c' de la ecuación de segundo grado:");
 
-        // Convertimos los coeficientes a números enteros
         int a = Integer.parseInt(aStr);
         int b = Integer.parseInt(bStr);
         int c = Integer.parseInt(cStr);
 
-        // Calculamos el discriminante de la ecuación
         double discriminante = Math.pow(b, 2) - 4 * a * c;
-
-        // Comprobamos si la ecuación tiene raíces reales o complejas
+        
         if (discriminante < 0) 
         {
             JOptionPane.showMessageDialog(null, "La ecuación tiene raíces complejas.");
